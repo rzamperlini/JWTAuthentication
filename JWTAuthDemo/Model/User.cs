467eb0c8dbb0e44e16abe1979a683650f5279d65
Rodrigo Zamperlini <rzamperlini@gmail.com>
2020-05-21 17:58:41 -0300

    Using data annotations.

--- JWTAuthDemo/Model/User.cs
+++ JWTAuthDemo/Model/User.cs
@@ -9,9 +9,19 @@ namespace JWTAuthDemo.Model
     public class User
     {
         [Key]
+        [Required]
         public int ID { get; set; }
+        
+        [Required(ErrorMessage = "An user name is required.")]
+        [StringLength(50)]
         public string UserName { get; set; }
+
+        [Required(ErrorMessage = "A password is required.")]
+        [StringLength(6)]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "An email address is required.")]
+        [StringLength(50)]
         public string EmailAddress { get; set; }
     }
 
